cfb3c317eb2d0da90829545b1c60a9405109534a
SarthakGirotra <sarthakgirotra@gmail.com>
2021-02-19 17:22:00 +0530

    axios url fix

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'http://192.168.0.111:3001/posts'
+const url = 'http://localhost:3001/posts'
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
